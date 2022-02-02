d480fd7fc7898c9b0cf12cffe22e0bba6c660d72
rohit11544 <brohit11544@gmail.com>
2022-02-03 01:04:16 +0530

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const url = "https://memories-server-code.herokuapp.com/posts";
+
+export const fetchPosts = () => axios.get(url);
+
+export const createPost = (newPost) => axios.post(url, newPost);
+
+export const updatePost = (id, updatePost) =>
+  axios.patch(`${url}/${id}`, updatePost);
+
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
